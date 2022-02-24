6117f54eb42388c2fdd3716cbac664bfe771cea9
Betul Bircan <bircan.betul_1996@hotmail.com>
2022-02-24 17:49:00 +0300

    circle.js dosyasındaki alan ve cevre hesaplama fonksiyonları index.js de çağrılsı.

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ circle.js dosyasına ulaşmak için iki yöntem vardır: 1.yöntem, değişkene 
 1.Yöntem-değişken -sayfaya circle.js dosyasını ekleme
 const daireHesaplama = require("./circle.js")
 
+-Alan ve çevre hesaplama Fonksiyonları çağırma
+daireHesaplama.circleArea(4)
+daireHesaplama.circleCircumference(4)
 
 */
 
 
 //2.yöntem-Object destructing-sayfaya circle dosyasını ekleme
 const {circleArea,circleCircumference} = require("./circle")
+
+//Alan ve çevre hesaplama Fonksiyonları çağırma
+circleArea(4)
+circleCircumference(4)
